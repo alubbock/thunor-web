b888f3a6e8a32b612c7abc8c40d561939416104c
Alex Lubbock <code@alexlubbock.com>
2016-11-21 21:45:20 -0600

    Switch to uWSGI

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 python manage.py migrate
 
-# Start Gunicorn processes
-exec gunicorn web.wsgi:application \
-    --name thunor \
-    --bind 0.0.0.0:8000 \
-    --workers $GUNICORN_WORKERS \
-    --log-level=info \
-    "$@"
+# Start uWSGI process
+uwsgi --socket :8000 --module web.wsgi --master --processes $UWSGI_PROCESSES --logto $UWSGI_LOGFILE
