b2afbe203695199fa06b1f36e933c6b222e45ee2
Alex Lubbock <code@alexlubbock.com>
2017-06-29 17:58:04 -0500

    Increase uWSGI buffer size for large URI query strings

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,4 @@ python manage.py migrate
 python manage.py collectstatic --no-input --ignore pyhts
 
 # Start uWSGI process
-uwsgi --socket :8000 --module web.wsgi --master --processes $UWSGI_PROCESSES --logto $UWSGI_LOGFILE --enable-threads --uid $THUNOR_USER --gid $THUNOR_GROUP
+uwsgi --socket :8000 -b 32768 --module web.wsgi --master --processes $UWSGI_PROCESSES --logto $UWSGI_LOGFILE --enable-threads --uid $THUNOR_USER --gid $THUNOR_GROUP
