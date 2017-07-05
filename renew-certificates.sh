56c2ec95a5790ed2c6fef20d4f2771e1a8237e61
Alex Lubbock <code@alexlubbock.com>
2017-07-05 15:25:48 -0500

    Remove renew certificates container on exit

--- renew-certificates.sh
+++ renew-certificates.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker-compose -f $THIS_DIR/docker-compose.certbot.yml run certbot certbot renew --non-interactive
+docker-compose -f $THIS_DIR/docker-compose.certbot.yml run --rm certbot certbot renew --non-interactive
