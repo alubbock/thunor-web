83887d704031297a88d660196731935b2e9ae04c
Alex Lubbock <code@alexlubbock.com>
2017-07-18 23:38:12 +0000

    Eager loading of uWSGI forks

--- web/wsgi.py
+++ web/wsgi.py
@@ -10,7 +10,13 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 from django.core.wsgi import get_wsgi_application
+from uwsgidecorators import postfork
+from django.test.client import Client
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
 
 application = Sentry(get_wsgi_application())
+
+@postfork
+def initialise():
+    Client().get('/')
