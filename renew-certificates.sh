0450222b6477a0eeb5adcc902a16b7c4d5596da8
Alex Lubbock <code@alexlubbock.com>
2017-10-11 14:47:24 -0500

    nginx reload hook for SSL cert renew script

--- renew-certificates.sh
+++ renew-certificates.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker-compose -f $THIS_DIR/docker-compose.certbot.yml run --rm --no-deps certbot certbot renew --non-interactive
+docker-compose -f $THIS_DIR/docker-compose.yml exec nginx nginx -s reload
