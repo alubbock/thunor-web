28223f36351cd381e932d9496e65217e82c2ce55
Alex Lubbock <code@alexlubbock.com>
2017-10-24 22:18:32 -0500

    Performance optimisations for multi-drug support
    Just the database-to-pandas adapter for now

--- pyhts/pandas.py
+++ pyhts/pandas.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from .models import Well, WellDrug, WellMeasurement, WellStatistic
-from django.db.models import Count, Max
+from django.db.models import Count, Max, Sum
 from collections import Iterable
 from pydrc.io import HtsPandas
 
@@ -20,11 +20,44 @@ def _add_int_or_list_filter(queryset, field_name, field_value):
     raise NotImplementedError()
 
 
-def _queryset_well_info(dataset_id, drug_id, cell_line_id):
+def _queryset_well_ids(dataset_id, drug_id, cell_line_id):
+    if isinstance(dataset_id, Iterable):
+        well_info = WellDrug.objects.filter(
+            well__plate__dataset_id__in=dataset_id
+        )
+    else:
+        well_info = WellDrug.objects.filter(
+            well__plate__dataset_id=dataset_id
+        )
+
+    well_info = well_info.annotate(num_drugs=Count('well__welldrug')).filter(
+        num_drugs=1)
+
+    well_info = _add_int_or_list_filter(well_info, 'drug_id', drug_id)
+    well_info = _add_int_or_list_filter(well_info, 'well__cell_line_id',
+                                        cell_line_id)
+
+    if drug_id and not cell_line_id:
+            well_info = well_info.order_by(
+             'well__cell_line__name', 'dose', 'well__plate_id',
+             'well__well_num')
+
+    well_info = well_info.annotate(max_dose=Max(
+        'well__welldrug__dose')).filter(max_dose__gt=0)
+
+    return well_info
+
+
+def _queryset_well_info(dataset_id, drug_id, cell_line_id,
+                        only_need_ids=False):
     drug_id = drug_id[0] if isinstance(drug_id, Iterable) and \
         len(drug_id) == 1 else drug_id
     cell_line_id = cell_line_id[0] if isinstance(cell_line_id, Iterable) and \
         len(cell_line_id) == 1 else cell_line_id
+
+    if not is_multi_drug_query(drug_id) and only_need_ids:
+        return _queryset_well_ids(dataset_id, drug_id, cell_line_id), \
+               drug_id, cell_line_id
 
     # Filter by dataset
     if isinstance(dataset_id, Iterable):
@@ -36,21 +69,17 @@ def _queryset_well_info(dataset_id, drug_id, cell_line_id):
             plate__dataset_id=dataset_id
         )
 
-    # well_info = well_info.annotate(num_drugs=Count('well__welldrug')).filter(
-    #     num_drugs=1)
-
     # Filter by cell line
-
-    # well_info = _add_int_or_list_filter(well_info, 'drug_id', drug_id)
     well_info_base = _add_int_or_list_filter(well_info_base,
                                              'cell_line_id',
                                              cell_line_id)
 
-    well_info_base = well_info_base.annotate(num_drugs=Count('welldrug'))
+    if drug_id is not None:
+        well_info_base = well_info_base.annotate(num_drugs=Count('welldrug'))
 
     # Filter control wells (these are fetched separately)
-    well_info_base = well_info_base.annotate(max_dose=Max(
-        'welldrug__dose')).filter(max_dose__gt=0)
+    well_info_base = well_info_base.annotate(total_dose=Sum(
+        'welldrug__dose')).filter(total_dose__gt=0)
 
     drug_ids_single = []
     drug_ids_combo = []
@@ -103,26 +132,24 @@ def _dataframe_wellinfo(dataset_id, drug_id, cell_line_id,
                                            'welldrug_set__drug')
     well_info = well_info.select_related('cell_line', 'plate')
     if use_dataset_names:
-        well_info = well_info.prefetch_related('plate__dataset')
+        well_info = well_info.select_related('plate__dataset')
 
-    x = []
-    for well in well_info:
-        vals = [(d.drug.name, d.dose) for d in well.welldrug_set.all()]
-        if vals:
-            names, doses = zip(*vals)
-
-            x.append({'dose': doses, 'well_id': well.id,
-             'cell_line': well.cell_line.name, 'drug': names, 'plate_id':
-                 well.plate_id,
-                      'dataset': dataset_id if not isinstance(dataset_id,
-                                                              Iterable)\
-                          and not use_dataset_names
-                      else (well.plate.dataset.name if use_dataset_names else
-                     well.plate.dataset_id)})
-
-    df_doses = pd.DataFrame(x)
-    df_doses.set_index(keys=['dataset', 'drug', 'cell_line', 'dose'],
-                       inplace=True)
+    df_doses = pd.DataFrame({'dose': tuple(d.dose for d in
+                                       well.welldrug_set.all()),
+                             'well_id':  well.id,
+                             'cell_line': well.cell_line.name,
+                             'drug': tuple(d.drug.name for d in
+                                      well.welldrug_set.all()),
+                             'plate_id': well.plate_id,
+                             'dataset': dataset_id if (not isinstance(
+                                 dataset_id, Iterable) and not
+                                use_dataset_names)
+                             else (well.plate.dataset.name if
+                                   use_dataset_names else
+                             well.plate.dataset_id)} for well in well_info)
+    if not df_doses.empty:
+        df_doses.set_index(keys=['dataset', 'drug', 'cell_line', 'dose'],
+                           inplace=True)
 
     return df_doses
 
@@ -135,7 +162,7 @@ def df_doses_assays_controls(dataset, drug_id, cell_line_id, assay):
 
     df_doses = _dataframe_wellinfo(dataset_id, drug_id, cell_line_id)
 
-    if df_doses.shape[0] == 3 and df_doses.isnull().values.all():
+    if df_doses.isnull().values.all():
         raise NoDataException()
 
     timecourses = WellMeasurement.objects.filter(
@@ -185,6 +212,14 @@ def df_doses_assays_controls(dataset, drug_id, cell_line_id, assay):
     return HtsPandas(df_doses, df_vals, df_controls)
 
 
+def is_multi_drug_query(drug_id):
+    # If drug ID is None, we assume the dataset may contain drug combos
+    return drug_id is None or (
+        isinstance(drug_id, Iterable) and
+        any(not isinstance(d, int) for d in drug_id)
+    )
+
+
 def df_dip_rates(dataset_id, drug_id, cell_line_id,
                  use_dataset_names=False):
     dip_stats = ('dip_rate', 'dip_fit_std_err')
@@ -192,72 +227,74 @@ def df_dip_rates(dataset_id, drug_id, cell_line_id,
     dataset_id_field = 'well__plate__dataset' + ('__name' if
                                                  use_dataset_names else '_id')
 
-    df_doses = _dataframe_wellinfo(dataset_id, drug_id, cell_line_id,
-                                   use_dataset_names=use_dataset_names)
+    well_info, drug_id, cell_line_id = _queryset_well_info(
+        dataset_id, drug_id, cell_line_id, only_need_ids=True)
 
-    # df_doses = queryset_to_dataframe(
-    #     WellStatistic.objects.filter(
-    #         well_id__in=well_info.values_list('well_id'),
-    #         stat_name__in=dip_stats,
-    #     ),
-    #     columns=('stat_name', 'value', 'well__welldrug__dose', 'well_id',
-    #              'well__cell_line__name', 'well__welldrug__drug__name',
-    #              dataset_id_field),
-    #     rename_columns=('stat_name', 'value', 'dose', 'well_id', 'cell_line',
-    #                     'drug', 'dataset')
-    # )
+    if is_multi_drug_query(drug_id):
+        # This query is complex, so we use list() to store the well_ids in
+        # Python before passing it to the two queries that use it
+        well_ids = list(well_info.values_list('id', flat=True).distinct())
 
-    if df_doses['well_id'].isnull().all():
-        raise NoDataException()
+        df_cl_dset = queryset_to_dataframe(
+            WellStatistic.objects.filter(
+                well_id__in=well_ids,
+                stat_name__in=dip_stats,
+            ),
+            columns=('stat_name', 'value', 'well_id'),
+            rename_columns=('stat_name', 'value', 'well_id')
+        )
 
-    well_stats = queryset_to_dataframe(
-        WellStatistic.objects.filter(
-            well_id__in=df_doses['well_id'],
-            stat_name__in=dip_stats,
-        ),
-        columns=('well_id', 'stat_name', 'value'),
-        index=('well_id', 'stat_name')
-    )
+        df_stats = df_cl_dset.pivot_table(index='well_id', columns='stat_name',
+                                          values='value')
 
-    if well_stats.isnull().values.all():
-        raise NoDataException
+        df_drugs = queryset_to_dataframe(
+            WellDrug.objects.filter(well_id__in=well_ids).order_by('well_id',
+                                                                   'order'),
+            columns=('well_id', 'drug__name', 'dose', 'well__cell_line__name',
+                     dataset_id_field),
+            rename_columns=('well_id', 'drug', 'dose', 'cell_line', 'dataset'),
+            index=('well_id', )
+        )
 
-    well_stats = well_stats.pivot_table(index='well_id', columns='stat_name',
-                                        values='value')
+        df_drugs = df_drugs.groupby('well_id').aggregate({
+            'drug': lambda x: tuple(x),
+            'dose': lambda x: tuple(x),
+            'dataset': lambda x: x,
+            'cell_line': lambda x: x
+        })
 
-    df_doses = pd.merge(df_doses, well_stats, left_on='well_id',
-                        right_index=True)
+        df_doses = pd.merge(df_stats, df_drugs, left_index=True, right_index=True)
+        df_doses.reset_index(inplace=True)
+        df_doses.set_index(['dataset', 'drug', 'cell_line', 'dose', 'well_id'],
+                           inplace=True)
+        df_doses.sort_index(inplace=True)
+    else:
+        # In the single drug case, we can do everything in a single query
+        df_doses = queryset_to_dataframe(
+            WellStatistic.objects.filter(
+                well_id__in=well_info.values_list('well_id').distinct(),
+                stat_name__in=dip_stats,
+            ),
+            columns=('stat_name', 'value', 'well__welldrug__dose', 'well_id',
+                     'well__cell_line__name', 'well__welldrug__drug__name',
+                     dataset_id_field),
+            rename_columns=(
+            'stat_name', 'value', 'dose', 'well_id', 'cell_line',
+            'drug', 'dataset')
+        )
 
-    # plate_ids = df_doses['plate_id'].unique()
-    # del df_doses['plate_id']
+        if df_doses.isnull().values.all():
+            raise NoDataException()
 
-    # df_doses = df_doses.pivot_table(
-    #     index=('dataset', 'drug', 'cell_line', 'dose', 'well_id'),
-    #     columns='stat_name', values=['value'])['value']
+        df_doses[['dose', 'drug']] = \
+            df_doses.transform({'dose': lambda x: (x, ),
+                                'drug': lambda x: (x, )})
 
-    # controls = WellStatistic.objects.filter(stat_name__in=dip_stats)
-    # if isinstance(dataset_id, Iterable):
-    #     controls = controls.filter(well__plate__dataset_id__in=dataset_id)
-    # else:
-    #     controls = controls.filter(well__plate__dataset_id=dataset_id)
-    # controls = _apply_control_filter(controls, cell_line_id)
-    #
-    # df_controls = queryset_to_dataframe(
-    #     controls,
-    #     columns=(dataset_id_field, 'well__cell_line__name',
-    #              'well_id', 'stat_name', 'value'),
-    #     rename_columns=('dataset', 'cell_line', 'well_id', 'stat_name',
-    #                     'value')
-    # )
-    #
-    # if df_controls.isnull().values.all():
-    #     df_controls = None
-    # else:
-    #     df_controls = df_controls.pivot_table(
-    #         index=('dataset', 'cell_line', 'well_id'),
-    #         columns='stat_name', values=['value'])['value']
+        df_doses = df_doses.pivot_table(
+            index=('dataset', 'drug', 'cell_line', 'dose', 'well_id'),
+            columns='stat_name', values=['value'])['value']
 
-    controls = WellStatistic.objects.filter(stat_name__in=dip_stats,)
+    controls = WellStatistic.objects.filter(stat_name__in=dip_stats)
     if isinstance(dataset_id, Iterable):
         controls = controls.filter(well__plate__dataset_id__in=dataset_id)
     else:
