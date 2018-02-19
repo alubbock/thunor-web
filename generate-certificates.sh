d1d9460679caa4b9da27b63fc68e036ded9bec63
Alex Lubbock <code@alexlubbock.com>
2018-02-19 17:39:28 -0600

    Move certbot config to separate compose file

--- generate-certificates.sh
+++ generate-certificates.sh
@@ -7,7 +7,7 @@ set -e
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "generating dhparams.pem, if required..."
-docker-compose -f $THIS_DIR/docker-compose.services.yml run --rm certbot sh -c "[[ -e /etc/letsencrypt/dhparams.pem ]] || openssl dhparam -out /etc/letsencrypt/dhparams.pem 2048"
+docker-compose -f $THIS_DIR/docker-compose.certbot.yml run --rm certbot sh -c "[[ -e /etc/letsencrypt/dhparams.pem ]] || openssl dhparam -out /etc/letsencrypt/dhparams.pem 2048"
 
 echo "generating requested certificates..."
-docker-compose -f $THIS_DIR/docker-compose.services.yml run --rm certbot certbot certonly --webroot --webroot-path /thunor-static "$@"
+docker-compose -f $THIS_DIR/docker-compose.certbot.yml run --rm certbot certbot certonly --webroot --webroot-path /thunor-static "$@"
--- renew-certificates.sh
+++ renew-certificates.sh
@@ -3,5 +3,5 @@ THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 : "${THUNORHOME:?"Need to set THUNORHOME environment variable"}"
 
-docker-compose -f $THIS_DIR/docker-compose.services.yml run --rm --no-deps certbot certbot renew --non-interactive
+docker-compose -f $THIS_DIR/docker-compose.certbot.yml run --rm --no-deps certbot certbot renew --non-interactive
 docker-compose exec nginx nginx -s reload
