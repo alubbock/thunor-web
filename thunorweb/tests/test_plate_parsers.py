fc5235655f32b08be21a9c0597ce4a807631dc19
Alex Lubbock <code@alexlubbock.com>
2018-02-25 00:43:41 -0600

    Tests for Vanderbilt CSV, HDF5 plate parsers

---
+++ thunorweb/tests/test_plate_parsers.py
@@ -0,0 +1,49 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.core.files import File
+import pkg_resources
+import thunor.io
+from thunorweb.models import HTSDataset
+from thunorweb.plate_parsers import PlateFileParser
+import io
+
+
+class TestPlateParsers(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create(email='test@example.com')
+        cls.d = HTSDataset.objects.create(name='test', owner=cls.user)
+
+    def test_parse_hdf(self):
+        filename = pkg_resources.resource_filename(
+            'thunor', 'testdata/VU001.h5')
+        with open(filename, 'rb') as src:
+            f = File(src)
+            pfp = PlateFileParser(f, dataset=self.d)
+            results = pfp.parse_all()
+
+        assert len(results) == 1
+        assert results[0]['success']
+        assert results[0]['file_format'] == 'HDF5'
+
+    def test_parse_vanderbilt_csv(self):
+        filename = pkg_resources.resource_filename(
+            'thunor', 'testdata/VU001.h5')
+
+        with io.StringIO() as csv_buffer:
+            # convert HDF to CSV in memory
+            thunor.io.write_vanderbilt_hts(
+                df_data=thunor.io.read_hdf(filename),
+                filename=csv_buffer,
+                sep=','
+            )
+
+            csv_buffer.seek(0)
+
+            pfp = PlateFileParser(File(csv_buffer, name='test.csv'),
+                                  dataset=self.d)
+            results = pfp.parse_all()
+
+        assert len(results) == 1
+        assert results[0]['success']
+        assert results[0]['file_format'] == 'Vanderbilt HTS Core'
