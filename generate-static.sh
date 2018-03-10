4fbe54f9fd30501f2e72d5d1bd174ceee6d202f6
Alex Lubbock <code@alexlubbock.com>
2018-03-10 13:17:46 -0600

    Move webpack directory

--- generate-static.sh
+++ generate-static.sh
@@ -8,12 +8,14 @@ BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ "$1" = "--no-container" ]; then
   echo "Processing staticfiles outside of container; do not use in production"
-  docker build -t thunor_webpack thunorweb/static
-  docker run --rm -v $THUNORHOME/_state/webpack-bundles:/_state/webpack-bundles thunor_webpack || exit $?
-  python $BASE_DIR/manage.py collectstatic --no-input --ignore thunorweb
+fi
+
+docker build -t thunor_webpack thunorweb/webpack
+docker run --rm -v $THUNORHOME/_state/webpack-bundles:/_state/webpack-bundles thunor_webpack
+
+if [ "$1" = "--no-container" ]; then
+  python $BASE_DIR/manage.py collectstatic --no-input
 else
-  docker build -t thunor_webpack thunorweb/static
-  docker run --rm -v $THUNORHOME/_state/webpack-bundles:/_state/webpack-bundles thunor_webpack || exit $?
-  docker-compose run --rm -v $THUNORHOME/_state/webpack-bundles:/thunor/_state/webpack-bundles -v $THUNORHOME/_state/thunor-static:/thunor/_state/thunor-static app python manage.py collectstatic --no-input --ignore thunorweb || exit $?
+  docker-compose run --rm -v $THUNORHOME/_state/webpack-bundles:/thunor/_state/webpack-bundles -v $THUNORHOME/_state/thunor-static:/thunor/_state/thunor-static app python manage.py collectstatic --no-input || exit $?
   echo "Put changes live with 'docker-compose up -d --build app'"
 fi
