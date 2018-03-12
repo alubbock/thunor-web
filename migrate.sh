b453ae01c01abac56e7bca4441119f55b3d97101
Alex Lubbock <code@alexlubbock.com>
2018-03-11 19:41:24 -0500

    Remove old scripts

--- migrate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ "$1" = "--dev" ]; then
-  python manage.py migrate || exit $?
-else
-  docker-compose run --rm app python manage.py migrate || exit $1
-fi
--- renew-certificates.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-: "${THUNORHOME:?"Need to set THUNORHOME environment variable"}"
-
-docker-compose -f $THIS_DIR/docker-compose.certbot.yml run --rm --no-deps certbot certbot renew --non-interactive
-docker-compose exec nginx nginx -s reload
