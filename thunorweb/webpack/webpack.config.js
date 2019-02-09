f1d37fef77b282456bdc09c18123a6620b9ce3cc
Alex Lubbock <code@alexlubbock.com>
2019-02-09 13:16:02 -0600

    Update JS build tools to webpack 4

--- thunorweb/webpack/webpack.config.js
+++ thunorweb/webpack/webpack.config.js
@@ -1,13 +1,16 @@
-var path = require("path");
-var webpack = require("webpack");
-var BundleTracker = require("webpack-bundle-tracker");
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var glob = require("glob");
+const path = require("path");
+const BundleTracker = require("webpack-bundle-tracker");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const glob = require("glob");
 const IgnoreUnchangedFilesPlugin = require("ignore-unchanged-webpack-plugin");
-var isDebug = (process.env.DJANGO_DEBUG === undefined ? false : process.env.DJANGO_DEBUG.toLowerCase() === "true");
+const isDebug = (process.env.DJANGO_DEBUG === undefined ? false : process.env.DJANGO_DEBUG.toLowerCase() === "true");
 
 var config = {
     context: __dirname,
+
+    mode: isDebug ? 'development' : 'production',
 
     entry: {
         favicons: glob.sync("./thunor/favicons/*"),
@@ -64,7 +67,10 @@ var config = {
     plugins: [
         new BundleTracker({path: "/thunor/_state/webpack-bundles/",
             filename: "webpack-stats.json"}),
-        new ExtractTextPlugin("[name]-[chunkhash].css"),
+        new MiniCssExtractPlugin({
+          filename: "[name]-[chunkhash].css",
+          chunkFilename: "[id]-[chunkhash].css"
+        }),
         new IgnoreUnchangedFilesPlugin()
     ],
 
@@ -78,22 +84,32 @@ var config = {
             },
             {
                 test: /\.css$/,
-                loader: ExtractTextPlugin.extract({
-                    loader: "css-loader",
-                    options: {minimize: !isDebug}
-                })
+                use: [{
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    "css-loader"]
             },
             {
                 test: /\.(png|ico)$/,
                 include: [
                     path.resolve(__dirname, "thunor/favicons"),
                 ],
-                loader: "file-loader?name=favicon/[name].[ext]"
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {name: 'favicon/[name].[ext]'}
+                    }
+                ]
             },
             {
                 test: /\.(png|jpg|gif|ico)$/,
                 exclude: /\/favicons\//,
-                loader: "file-loader?name=img/[name]-[hash].[ext]"
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {name: 'img/[name]-[hash].[ext]'}
+                    }
+                ]
             },
             {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff&name=font/[name]-[hash].[ext]"},
             {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/octet-stream&name=font/[name]-[hash].[ext]"},
@@ -112,26 +128,21 @@ var config = {
             "jquery-ui-js": "jquery-ui/ui",
             "jquery-ui-css": "jquery-ui/themes/base"
         }
+    },
+
+    optimization: {
+      minimizer: [
+        new UglifyJsPlugin({
+          cache: true,
+          parallel: true,
+          sourceMap: true // set to true if you want JS source maps
+        }),
+        new OptimizeCSSAssetsPlugin({})
+      ]
     }
 };
 
 if (!isDebug) {
-    config.plugins.push(
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                screw_ie8: true,
-                warnings: false
-            },
-            mangle: true,
-            sourceMap: true
-        })
-    );
-    config.plugins.push(
-        new webpack.SourceMapDevToolPlugin({
-            filename: "[file].map",
-            include: /^app/
-        })
-    );
     var CompressionPlugin = require("compression-webpack-plugin");
     config.plugins.push(
         new CompressionPlugin({
