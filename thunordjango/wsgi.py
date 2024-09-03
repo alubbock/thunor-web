8888fcf1d643feb1f279a01c874e33179ed5204f
Alex Lubbock <code@alexlubbock.com>
2024-09-03 17:24:46 +0100

    fix: disable wsgi warmup (#57)

--- thunordjango/wsgi.py
+++ thunordjango/wsgi.py
@@ -8,19 +8,8 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-import sys
 from django.core.wsgi import get_wsgi_application
-from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thunordjango.settings")
 
 application = get_wsgi_application()
-
-application({
-    'REQUEST_METHOD': 'GET',
-    'SERVER_NAME': '127.0.0.1',
-    'SERVER_PORT': 8080,
-    'PATH_INFO': '/warmup/',
-    'HTTP_HOST': settings.HOSTNAME,
-    'wsgi.input': sys.stdin,
-}, lambda x, y: None)  # call the entry-point function
